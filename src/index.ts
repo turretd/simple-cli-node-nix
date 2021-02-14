e3a15c6fe0593a02cebbe87027c4fac127a76014
turretd <hello@turretd.no>
2021-02-15 00:21:31 +0100

    plural

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ const argv = yargs(hideBin(process.argv)).argv
 if (argv.ships > 3 && argv.distance < 53.5) {
   console.log('Run to the deck!')
 } else {
-  console.log('Oh no, I dropped my devil fruit!')
+  console.log('Oh no, I dropped my devil fruits!')
 }

28deba126e82630f1c1a2fa6523cc998ad850d22
turretd <69400632+turretd@users.noreply.github.com>
2021-02-15 06:23:51 +0100

    Update index.ts

--- packages/simple-cli/src/index.ts
+++ packages/simple-cli/src/index.ts
@@ -5,6 +5,7 @@ const argv = yargs(hideBin(process.argv)).argv
 
 if (argv.ships > 3 && argv.distance < 53.5) {
   console.log('Run to the deck!')
+  console.log('Bring the dawn to the future!')
 } else {
   console.log('Oh no, I dropped my devil fruits!')
 }

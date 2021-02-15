0fd230a85b63e0a69b0657816129b873032b33a2
turretd <hello@turretd.no>
2021-02-15 06:18:52 +0100

    moving towards reproducibility

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
-{pkgs ? import <nixpkgs> {}, ...}:
-
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ff96a0fa5635770390b184ae74debea75c3fd534.tar.gz") {}, ...}:
 let
-  nodeDependencies = (pkgs.callPackage ./node2nix.nix {}).shell.nodeDependencies;
+  nodeDependencies = (pkgs.callPackage ./packages/simple-cli/node2nix.nix {}).shell.nodeDependencies;
 in
 pkgs.stdenv.mkDerivation {
   name = "simple-cli";
-  src = ./.;
+  src = ./packages/simple-cli;
   buildInputs = with pkgs; [nodejs yarn];
   buildPhase = ''
     ln -s ${nodeDependencies}/lib/node_modules ./node_modules
--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ff96a0fa5635770390b184ae74debea75c3fd534.tar.gz") {}, ...}: pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs
     yarn
